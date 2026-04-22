4ba060880febb507b1caf9dbb7a625fb4d733697
Вита <@DESKTOP-FQK0QE3>
2026-04-22 23:07:34 +0700

    Добавьте файлы проекта.

---
+++ kniga_book_teliphony/Models/Contact.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kniga_book_teliphony.ViewModels;
+
+namespace kniga_book_teliphony.Models
+{
+    public class Contact : ObservableObject
+    {
+        private string _name = string.Empty;
+        private string _phone = string.Empty;
+
+        // Допишите конструктор, который вызовет метод Validate для проверки значений.
+        // Если некорректные – выбросить исключение
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                // TODO: допишите установку _name и вызов
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+                // события изменения свойства
+
+            }
+        }
+        public string Phone
+        {
+            get => _phone;
+            set
+            {
+                // TODO: допишите установку _phone и вызов
+                if (_phone != value)
+                {
+                    _phone = value;
+                    OnPropertyChanged();
+                }
+                // события изменения свойства
+            }
+        }
+        // TODO: добавьте метод Validate(), который
+        // проверяет, что Name не пуст и Phone
+        // соответствует формату +7XXXXXXXXXX (или без кода страны)
+        // Метод должен возвращать bool
+        public bool Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Phone))
+            {
+                return false;
+            }
+
+            if (Phone.Length == 12 && Phone.StartsWith("+7"))
+            {
+                for (int i = 2; i < Phone.Length; i++)
+                {
+                    if (!char.IsDigit(Phone[i]))
+                        return false;
+                }
+                return true;
+            }
+
+            if (Phone.Length == 10)
+            {
+                foreach (char c in Phone)
+                {
+                    if (!char.IsDigit(c))
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ kniga_book_teliphony/ViewModels/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using kniga_book_teliphony.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace kniga_book_teliphony.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        // Коллекция контактов
+        public ObservableCollection<Contact> Contacts { get; set; }
+        private string _name = string.Empty;
+        private string _phone = string.Empty;
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+        public string Phone
+        {
+            get => _phone;
+            set => Set(ref _phone, value);
+        }
+
+        private Contact? _selectedContact;
+        public Contact? SelectedContact
+        {
+            get => _selectedContact;
+            set => Set(ref _selectedContact, value);
+        }
+        // Команды
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+            AddCommand = new RelayCommand(
+            AddContact,() => CanAddContact());
+            DeleteCommand = new RelayCommand(
+            DeleteContact,() => CanDeleteContact());
+        }
+        private void AddContact()
+        {
+            Contact newContact = new Contact
+            {
+                Name = Name,
+                Phone = Phone,
+            };
+            Contacts.Add(newContact);
+            Name = string.Empty;
+            Phone = string.Empty;
+        }
+        private bool CanAddContact()
+        {
+            if (Name != null) { return true; }
+            if (Phone != null) { return true; }
+            return false; // временная заглушка
+        }
+        private void DeleteContact()
+        {
+            if (SelectedContact != null)
+            Contacts.Remove(SelectedContact);
+        }
+        private bool CanDeleteContact()
+        {
+            if (SelectedContact != null) { return true; }
+            return false; // временная заглушка
+        }
+    }
+}
